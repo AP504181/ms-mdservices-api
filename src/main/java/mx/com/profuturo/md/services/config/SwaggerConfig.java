174a534eb40ada98404c4eab24910e95387d3815
Alfredo Pérez Mendoza <alfredo.perez1@profuturo.com.mx>
2026-01-27 15:10:38 -0600

    update corss final

--- src/main/java/mx/com/profuturo/md/services/config/SwaggerConfig.java
+++ src/main/java/mx/com/profuturo/md/services/config/SwaggerConfig.java
@@ -2,10 +2,12 @@ package mx.com.profuturo.md.services.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,6 +16,11 @@ public class SwaggerConfig {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.components(new Components())
+				.servers(List.of(
+						new Server()
+								.url("https://ms-mdservices-api-production-c2d7.up.railway.app/md-services")
+								.description("Railway Production")
+				))
 				.info(new Info()
 						.title("Modificacion Datos MD API")
 						.description("Especificación técnica para la modificacion de datos")
